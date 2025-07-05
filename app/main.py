988972433c6f0ec3a203b0ea40d960464ed0fc44
Twoje Imię <mate-filip@github.com>
2025-07-05 20:13:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
+        return
